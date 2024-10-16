908e708e1b591bd4827799aa5defad811665a074
Spentamone <jesusportal3535@gmail.com>
2024-10-15 23:11:51 -0500

    Delete src directory

--- src/AIntPastelManager.java
+++
@@ -1,6 +0,0 @@
-import java.math.BigDecimal;
-
-public interface AIntPastelManager {
-    int agregarPastel(String nombre, boolean dietFriendly, BigDecimal precio);
-    void agregarDescripcion(int idPastel, String descripcion);
-}
