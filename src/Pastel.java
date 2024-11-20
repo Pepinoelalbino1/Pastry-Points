0f6f37ac8d789a92e42419e1016b30445748d81a
Spentamone <jesusportal3535@gmail.com>
2024-11-19 23:54:56 -0500

    Delete src directory

--- src/Pastel.java
+++
@@ -1,9 +0,0 @@
-public interface Pastel extends Cloneable {
-    Pastel clonar(); // Método para clonar el pastel
-    int getId();
-    String getNombre();
-    boolean isDietFriendly();
-    double getPrecio();
-    int getCalificacion();
-    void actualizarCalificacion(boolean recomendacion);
-}
